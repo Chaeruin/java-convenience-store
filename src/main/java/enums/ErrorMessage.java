058e49fc99a7a7babc0a0b8581941ef60e0fc8b0
Chaeruin <cms3604@naver.com>
2024-11-28 11:00:16 +0900

    docs: 기능 요구사항 분석 완료 및 기능 구현 목록 작성 in README.md

---
+++ src/main/java/enums/ErrorMessage.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum ErrorMessage {
+
+    INVALID_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요."),
+    INVALID_OVER_STOCK("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    INVALID_NOT_EXISTED_PRODUCT("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    INVALID_FORMATTING("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+}
